4cd8c042b2c2a1a94d1db4468db8ddfb9497869f
zevans98 <zman6221998@gmail.com>
2018-11-29 16:18:08 -0700

    heroku

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "lion",
+		photo: "https://kidzfeed.com/wp-content/uploads/2018/08/Lion-Facts-for-kids-e1534255925789.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "simba",
+		photo: "https://kidzfeed.com/wp-content/uploads/2018/08/Lion-Facts-for-kids-e1534255925789.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "mufasa",
+		photo: "https://kidzfeed.com/wp-content/uploads/2018/08/Lion-Facts-for-kids-e1534255925789.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "lion guy",
+		photo: "https://kidzfeed.com/wp-content/uploads/2018/08/Lion-Facts-for-kids-e1534255925789.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "lyon",
+		photo: "https://kidzfeed.com/wp-content/uploads/2018/08/Lion-Facts-for-kids-e1534255925789.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
